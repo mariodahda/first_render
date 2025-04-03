fdeaa7ec50d4bf2aa89ee55691c34eee27197ec8
Mario Alberto Dahada Martinez <mariodahda@Mac.lan>
2025-04-03 17:25:09 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+ # Esta es una línea en blanco
+st.header('Lanzar una moneda')
+ # Esta es una línea en blanco
+st.write('Esta aplicación aún no es funcional. En construcción.')
